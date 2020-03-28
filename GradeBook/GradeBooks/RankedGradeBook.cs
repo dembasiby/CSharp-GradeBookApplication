1282b7bcf98574ab034cbfaf02943995af3978f2
Demba Siby <dembasiby@gmail.com>
2020-03-28 16:49:24 +0000

    Test GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +10,41 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count() < 5)
+                throw new InvalidOperationException();
+
+            var sortedGrades = Students.OrderByDescending(s => s.AverageGrade)
+                                                 .Select(s => s.AverageGrade);
+            var twentyPercent = sortedGrades.Count() / 4;
+
+            if (sortedGrades.Take(twentyPercent).Contains(averageGrade))
+            {
+                return 'A';
+            }
+            else if (sortedGrades.Skip(twentyPercent)
+                                 .Take(twentyPercent)
+                                 .Contains(averageGrade))
+            {
+                return 'B';
+            }
+            else if (sortedGrades.Skip(twentyPercent * 2)
+                                 .Take(twentyPercent)
+                                 .Contains(averageGrade))
+            {
+                return 'C';
+            }
+            else if (sortedGrades.Skip(twentyPercent * 3)
+                                 .Take(twentyPercent)
+                                 .Contains(averageGrade))
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
